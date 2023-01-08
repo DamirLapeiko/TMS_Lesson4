698d33b6bf78006d4550bcee6ddeaea85fdadcc6
DamirLapeiko <lapeiko02@mail.ru>
2023-01-08 16:52:53 +0300

    Homework

--- src/Computer/TestingComputer.java
+++ src/Computer/TestingComputer.java
@@ -2,13 +2,13 @@ package Computer;
 
 public class TestingComputer {
     public static void main(String[] args) {
-        Computer comp1 = new Computer(2500,"HP", new RAM(), new HDD());
+        Computer comp1 = new Computer(2500,"HP");
         RAM ram1 = new RAM();
         HDD hdd1 = new HDD();
         comp1.info();
         ram1.infoRAM();
         hdd1.infoHDD();
-        Computer comp2 = new Computer(5000,"Lenovo", new RAM(), new HDD());
+        Computer comp2 = new Computer(5000,"Lenovo");
         RAM ram2 = new RAM("HyperX", 8);
         HDD hdd2 = new HDD("Seagate", 1000,"external");
         comp2.info();
